6dcabd0a16e17956d7a7f10bf04ac90ac030ebcc
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-01-24 23:31:06 +0100

    Started project

---
+++ goffrey.go
@@ -0,0 +1,15 @@
+/*
+    Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+    Project       Goffrey
+    Description   A simple IPAM
+    License       GPL version 2 (see GPL.txt for details)
+*/
+
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
