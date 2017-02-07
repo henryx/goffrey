deb782f50f74393804ffc268dca1eac1e278a788
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-02-08 00:00:33 +0100

    Formatted code

--- goffrey.go
+++ goffrey.go
@@ -1,10 +1,9 @@
 /*
-    Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
-    Project       Goffrey
-    Description   A simple IPAM
-    License       GPL version 2 (see GPL.txt for details)
+   Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       Goffrey
+   Description   A simple IPAM
+   License       GPL version 2 (see GPL.txt for details)
 */
-
 
 package main
 
