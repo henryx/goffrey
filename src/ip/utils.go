76887967b91f7b708bda684becaeddf605a6e608
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-02-07 23:59:52 +0100

    Added code for getting a range of IP addresses

--- src/ip/utils.go
+++ src/ip/utils.go
@@ -9,6 +9,15 @@ package ip
 
 import "net"
 
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
 func ToCidr(mask string) int {
 	netmask := net.IPMask(net.ParseIP(mask).To4()) // If you have the mask as a string
 	//netmask := net.IPv4Mask(255,255,255,0) // If you have the mask as 4 integer values
@@ -16,4 +25,18 @@ func ToCidr(mask string) int {
 	prefix, _ := netmask.Size()
 	return prefix
 
+}
+
+func Range(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// remove network address and broadcast address
+	return ips[1: len(ips)-1], nil
 }
