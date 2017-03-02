3427cf5c9f10eadad839acecb58fa9e4c851b5d0
Enrico Bianchi <enrico.bianchi@par-tec.it>
2017-03-02 16:43:49 +0100

    Populated result struct

--- src/args/add.go
+++ src/args/add.go
@@ -5,9 +5,25 @@
    License       GPL version 2 (see GPL.txt for details)
 */
 
-
 package args
 
-func parseaddargs(res *Args, args []string) {
+import (
+	"net"
+	"errors"
+	"ip"
+)
 
+func parseaddargs(res *Args, args []string) error {
+	res.Name = args[0]
+
+	if ok := net.ParseIP(args[0]); ok != nil {
+		res.Network = args[1]
+	} else {
+		return errors.New("IP address not valid: " + args[0])
+	}
+
+	cidr := ip.ToCidr(args[2])
+	res.Netmask = cidr
+
+	return nil
 }
