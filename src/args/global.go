04d7e41ba2c35068e3d6e490e17d18b7db9cf3f4
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-03-04 15:57:52 +0100

    Used a variable instead of a constant for getting slice element

--- src/args/global.go
+++ src/args/global.go
@@ -30,10 +30,10 @@ type Args struct {
 	Netmask int
 }
 
-
-
 func Parse() (Args, error) {
 	res := Args{}
+	command := 1
+
 	if len(os.Args) <= 1 || utils.ContainStr(os.Args, "-h") || utils.ContainStr(os.Args, "--help") {
 		fmt.Println(help)
 		os.Exit(0)
@@ -42,17 +42,17 @@ func Parse() (Args, error) {
 			return Args{}, errors.New("Not enough parameters (see \"-h\" option)")
 		}
 
-		switch os.Args[1] {
+		switch os.Args[command] {
 		case "help":
-			parsehelpargs(os.Args[2])
+			parsehelpargs(os.Args[command])
 		case "add":
 			res.Action = "add"
-			if err := parseaddargs(&res, os.Args[2:]); err != nil {
+			if err := parseaddargs(&res, os.Args[command+1:]); err != nil {
 				return Args{}, err
 			}
 		case "del":
 			res.Action = "del"
-			parsedelargs(&res, os.Args[2:])
+			parsedelargs(&res, os.Args[command+1:])
 		}
 	}
 
