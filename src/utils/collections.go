3a5ae02d0276cb100178bcb04bde1e6e6525e713
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-03-04 15:29:25 +0100

    Added pacakge for managing collections

---
+++ src/utils/collections.go
@@ -0,0 +1,9 @@
+/*
+   Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       Goffrey
+   Description   A simple IPAM
+   License       GPL version 2 (see GPL.txt for details)
+*/
+
+
+package utils
