bb3ac618c26fede2a4fbee48aa9cf59d23a95621
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-03-18 18:12:38 +0100

    Added code for checking how database connection was opened

--- src/actions/register.go
+++ src/actions/register.go
@@ -7,7 +7,12 @@
 
 package actions
 
-import "github.com/go-ini/ini"
+import (
+	"github.com/go-ini/ini"
+	"database/sql"
+	"dbstore"
+	"errors"
+)
 
 type RegisterData struct {
 	Enable  bool
@@ -16,7 +21,33 @@ type RegisterData struct {
 	Netmask string `names:"-M, --netmask" usage:"Set the network mask"`
 }
 
-func Register(cfg *ini.File, data RegisterData) {
+func Register(cfg *ini.File, data RegisterData) (error) {
+	var db *sql.DB
+	var err error
 
+	dbtype := cfg.Section("general").Key("dbstore").String()
+	switch dbtype {
+	case "sqlite":
+		location := cfg.Section("sqlite").Key("location").String()
+		db, err = dbstore.OpenSQLite(location)
+	case "postgresql":
+		dbhost := cfg.Section("postgresql").Key("host").String()
+		dbport, err := cfg.Section("postgresql").Key("port").Int()
+		if err != nil {
+			return errors.New("Invalid PostgreSQL port: " + err.Error())
+		}
+		dbuser := cfg.Section("postgresql").Key("user").String()
+		dbpassword := cfg.Section("postgresql").Key("password").String()
+		dbdatabase := cfg.Section("postgresql").Key("database").String()
+		db, err = dbstore.OpenPostgres(dbhost, dbport, dbuser, dbpassword, dbdatabase)
+	default:
+		return errors.New("Database not supported: " + dbtype)
+	}
+
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	return nil
 }
-
