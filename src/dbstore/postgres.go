8dea63db63bf5ad620d66a29c11416ed188e244f
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-04-12 00:32:43 +0200

    Added code for checking and creating database schema in PostgreSQL database

--- src/dbstore/postgres.go
+++ src/dbstore/postgres.go
@@ -9,10 +9,45 @@ package dbstore
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
+	"log"
 	"strings"
 	"strconv"
+	_ "github.com/lib/pq"
 )
+
+func isPgDbExists(db *sql.DB) bool {
+	var counted int
+
+	query := "SELECT Count(*) FROM information_schema.tables " +
+		"WHERE table_schema = 'public' " +
+		"AND table_name in ('status', 'attrs', 'acls')"
+
+	err := db.QueryRow(query).Scan(&counted)
+	if err != nil {
+		log.Fatal("Schema 1: Error in check database structure: " + err.Error())
+	}
+
+	if counted > 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func createPgDb(db *sql.DB) {
+	var tx *sql.Tx
+
+	tx, _ = db.Begin()
+	for _, query := range tables() {
+		_, err := tx.Exec(query)
+		if err != nil {
+			tx.Rollback()
+			log.Fatal(err)
+			break
+		}
+	}
+	tx.Commit()
+}
 
 func OpenPostgres(host string, port int, user string, password string, database string) (*sql.DB, error) {
 	dsn := strings.Join([]string{
@@ -27,6 +62,10 @@ func OpenPostgres(host string, port int, user string, password string, database 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
+	}
+
+	if !isPgDbExists(db) {
+		createPgDb(db)
 	}
 
 	return db, nil
