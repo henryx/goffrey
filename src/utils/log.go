a9e31c58be1a713b0fc3fc058a43c9da93c1d707
Enrico Bianchi <enrico.bianchi@gmail.com>
2017-05-16 23:33:54 +0200

    Updated header

--- src/utils/log.go
+++ src/utils/log.go
@@ -1,8 +1,8 @@
 /*
-Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
-Project       dbtester
-Description   A simple database tester
-License       GPL version 2 (see GPL.txt for details)
+   Copyright (C) 2017 Enrico Bianchi (enrico.bianchi@gmail.com)
+   Project       Goffrey
+   Description   A simple IPAM
+   License       GPL version 2 (see GPL.txt for details)
 */
 
 package utils
